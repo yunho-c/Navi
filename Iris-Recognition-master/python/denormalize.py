772f52187eff29449d150f1d39690fe754d9e943
k2m5t2 <yunhoyunho@gatech.edu>
2022-03-07 00:51:03 -0500

    Denormalization Work 4

--- Iris-Recognition-master/python/denormalize.py
+++ Iris-Recognition-master/python/denormalize.py
@@ -7,6 +7,11 @@ from detail_extraction import erase_non_iris, detail_extraction
 radial_res = 20
 angular_res = 240
 
+GAIN = 5
+
+fn1 = '001_1_2.bmp'
+fn2 = '002_1_1.bmp' # ?
+
 
 def draw_circle(img, cntr, r, val): 
     y, x = np.ogrid[:img.shape[0], :img.shape[1]]
@@ -80,28 +85,59 @@ def main():
     from cv2 import imread #, imwrite
 
     # read, segment, clean sample image
-    im1 = imread('001_1_2.bmp', 0)
+    im1 = imread(fn1, 0)
     eyelashes_thres = 80; use_multiprocess = False
     cir_iris, cir_pupil, imwithnoise = segment(im1, eyelashes_thres, use_multiprocess)
     iris = erase_non_iris(cir_iris, imwithnoise)
     detail = detail_extraction(cir_iris, imwithnoise)
     result = im1 - np.nan_to_num(detail)
+    result2_sp = result.copy()
     # test: add normalized -> denormalized detail from same file
     polar_array, noise_array = normalize(detail, cir_iris[1], cir_iris[0], cir_iris[2],
                                         cir_pupil[1], cir_pupil[0], cir_pupil[2],
                                         radial_res, angular_res)
     denormalized_detail = denormalize(cir_iris, cir_pupil, iris, polar_array)
-    result = result + (5/np.max(denormalized_detail + np.min(denormalized_detail)))*denormalized_detail
+    result = result + (GAIN/np.max(denormalized_detail + np.min(denormalized_detail)))*denormalized_detail
 
-    min, max = np.min(denormalized_detail), np.max(denormalized_detail)
-    plt.title('Denormalized Detail: Radial Resolution={}, Angular Resolution: {}, Zmin: {}, Zmax: {}'.format(radial_res, angular_res, min, max), fontweight ="bold")
+    plt.title('Iris Detail Recovered from Original Normalized Template'.format(radial_res, angular_res), fontweight ="bold")
     plt.imshow(denormalized_detail)
+    min, max = np.min(denormalized_detail), np.max(denormalized_detail)
+    plt.text(15, 15, "Zmin: {}, Zmax: {}".format(round(min, 2), round(max, 2)))
+    plt.text(15, 30, "Radial Resolution={}, Angular Resolution: {}".format(radial_res, angular_res))
     plt.show()
+    
 
-    plt.title('Result: Radial Resolution={}, Angular Resolution: {}'.format(radial_res, angular_res), fontweight ="bold")
+    plt.title('Original Iris Detail Removed, Normalized, Recovered, Added'.format(radial_res, angular_res), fontweight ="bold")
+    plt.text(15, 15, "Radial Resolution={}, Angular Resolution: {}".format(radial_res, angular_res))
+    plt.text(15, 30, "Gain={}".format(GAIN))
     plt.imshow(result)
     plt.show()
 
+    im2 = imread(fn2, 0)
+
+    cir_iris2, cir_pupil2, imwithnoise2 = segment(im2, eyelashes_thres, use_multiprocess)
+    iris2 = erase_non_iris(cir_iris2, imwithnoise2)
+    detail2 = detail_extraction(cir_iris2, imwithnoise2)
+    result2 = im2 - np.nan_to_num(detail2)
+    # test: add normalized -> denormalized detail from same file
+    polar_array2, noise_array2 = normalize(detail2, cir_iris2[1], cir_iris2[0], cir_iris2[2],
+                                        cir_pupil2[1], cir_pupil2[0], cir_pupil2[2],
+                                        radial_res, angular_res)
+    denormalized_detail2 = denormalize(cir_iris, cir_pupil, iris, polar_array2)
+    result2 = result2_sp + (GAIN/np.max(denormalized_detail2 + np.min(denormalized_detail2)))*denormalized_detail2
+
+    plt.title('Iris Detail Recovered from Distinct Normalized Template')
+    plt.text(15, 15, "Original Image={}, Iris Detail: {}".format(fn1, fn2))
+    min, max = np.min(denormalized_detail), np.max(denormalized_detail)
+    plt.text(15, 15, "Zmin: {}, Zmax: {}".format(round(min, 2), round(max, 2)))
+    plt.imshow(denormalized_detail2)
+    plt.show()
+
+    plt.title('Original Iris Detail Removed, Detail Recovered from Distinct Normalized Template Added')
+    plt.text(15, 15, "Original Image={}, Iris Detail: {}".format(fn1, fn2))
+    plt.text(15, 30, "Gain={}".format(GAIN))
+    plt.imshow(result2)
+    plt.show()
 
 
     # test: add detail from another file
