345656d6adf8af2038d87ec738491ee8b787d7e5
k2m5t2 <yunhoyunho@gatech.edu>
2022-03-06 22:15:01 -0500

    denormalization work 3

--- Iris-Recognition-master/python/denormalize.py
+++ Iris-Recognition-master/python/denormalize.py
@@ -1,3 +1,4 @@
+from matplotlib.pyplot import polar
 import numpy as np
 
 from fnc.normalize import normalize
@@ -13,8 +14,9 @@ def draw_circle(img, cntr, r, val):
     return np.where(dist <= r, img, val)
 
 def smart_r_grid(cir_iris, cir_pupil, iris_bool):
-    cntr_iris =  cir_iris[0:2]; r_iris = cir_iris[2]
-    cntr_diff = cir_pupil[0:2] - cir_iris[0:2]; r_diff = cir_pupil[2] - cir_iris[2]
+    cntr_iris = cir_iris[0:2]; r_iris = cir_iris[2]
+    cntr_diff = np.array(cir_pupil[0:2]) - np.array(cir_iris[0:2]) 
+    r_diff = cir_pupil[2] - cir_iris[2]
 
     r_grid = iris_bool.copy().astype(int)
     for i in range(20):
@@ -25,32 +27,44 @@ def smart_r_grid(cir_iris, cir_pupil, iris_bool):
 
 
 def denormalize(cir_iris, cir_pupil, im_iris, normalized_template):
-    # iris = np.array(im_iris, dtype=bool) # note: boolean
-    iris = im_iris.astype(bool) # 이거 될까?
+    # iris = np.nan_to_num(im_iris).astype(bool) # boolean.
+    iris = ~np.isnan(im_iris)
+    # iris_nan = iris_bool.copy()
 
     # dense grid coordinate values
     y_grid, x_grid = np.mgrid[0:iris.shape[0], 0:iris.shape[1]]
 
-    # if it's not iris, replace with NaN
-    y_grid = np.where(not iris, y_grid, np.nan) 
-    x_grid = np.where(not iris, x_grid, np.nan)
+    # if it's not iris, replace with 0
+    # ideally NaN though
+    y_grid = np.where(iris, y_grid, 0)
+    x_grid = np.where(iris, x_grid, 0)
 
     # zero into the iris center -- relative coordinates
     cntr_iris = cir_iris[0:2]; r_iris = cir_iris[2]
-    y_c_grid = y_grid - cntr_iris[1]
-    x_c_grid = x_grid - cntr_iris[0]
+    y_c_grid = y_grid - cntr_iris[0]
+    # y_c_grid = -y_c_grid # flip signs
+    x_c_grid = x_grid - cntr_iris[1]
 
     # use it to derive a matrix representing r and a matrix 
     # representing theta per each point in original space.
     r_grid = np.sqrt(y_c_grid**2 + x_c_grid**2)
-    th_grid = np.arctan2(y_c_grid/x_c_grid)
+    th_grid = np.arctan2(y_c_grid, x_c_grid)
 
     # scaling and discretization for coordinate alignment
-    r_grid = radial_res/r_iris * r_grid.astype(int) # r -> y
-    effective_r_grid = smart_r_grid(cir_iris, cir_pupil, iris) # r -> y
-    th_grid = angular_res/(2*np.pi) * th_grid.astype(int) # th -> x
+    r_grid = (radial_res/r_iris * r_grid).astype(int) # r -> y
+    e_r_grid = smart_r_grid(cir_iris, cir_pupil, iris) # r -> y
+    th_grid = (angular_res/(2*np.pi) * th_grid).astype(int) # th -> x
 
-    normalized_template[th_grid, r_grid]
+    # last-minute data adjustment: boundary case, zero-norm
+    r_grid -= 1; th_grid -= 1
+    th_grid += angular_res//2
+
+    # remove background offset # TEMPORARY
+    r_grid = np.where(iris, r_grid, 0)
+    th_grid = np.where(iris, th_grid, 0)
+
+
+    a = normalized_template[r_grid, th_grid]
 
     return 
 
@@ -66,9 +80,17 @@ def main():
     cir_iris, cir_pupil, imwithnoise = segment(im1, eyelashes_thres, use_multiprocess)
     iris = erase_non_iris(cir_iris, imwithnoise)
     detail = detail_extraction(cir_iris, imwithnoise)
+    result = im1 - np.nan_to_num(detail)
     # test: add normalized -> denormalized detail from same file
-    result = ''
-    normalize
+    polar_array, noise_array = normalize(detail, cir_iris[1], cir_iris[0], cir_iris[2],
+                                        cir_pupil[1], cir_pupil[0], cir_pupil[2],
+                                        radial_res, angular_res)
+    denormalized_detail = denormalize(cir_iris, cir_pupil, iris, polar_array)
+    result = result + denormalized_detail
+
+    plt.imshow(result)
+    plt.show()
+
     # test: add detail from another file
     # hamming distance
     # image comparison (50/50)
